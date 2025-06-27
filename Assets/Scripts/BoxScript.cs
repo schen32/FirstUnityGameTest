eb3eea1163b6462b6bdf2f96dc80736f761cea9d
Simon Chen <simonc8192@gmail.com>
2025-06-26 21:04:05 -0700

    - tweaked player movement

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -12,13 +12,14 @@ public class BoxScript : MonoBehaviour
     public float fallingGravityScale;
     Vector2 moveDir;
     bool canJump;
+    bool playerMoved;
     SpriteRenderer m_spriteRenderer;
     Rigidbody2D m_rigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveSpeed = 500.0f;
-        jumpHeight = 2000.0f;
+        jumpHeight = 1500.0f;
         rotationSpeed = 10.0f;
         gravityScale = 10.0f;
         fallingGravityScale = 20.0f;
@@ -32,12 +33,25 @@ public class BoxScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveVelocityX = moveDir.x * moveSpeed * Time.deltaTime;
-        float moveVelocityY = moveDir.y * jumpHeight * Time.deltaTime;
+        float moveVelocityX = 0;
+        float angularVelocity = 0;
+        if (moveDir.x > 0)
+        {
+            moveVelocityX = moveSpeed * Time.deltaTime;
+            angularVelocity = rotationSpeed * Time.deltaTime;
+            
+        }
+        else if (moveDir.x < 0)
+        {
+            moveVelocityX = -moveSpeed * Time.deltaTime;
+            angularVelocity = -rotationSpeed * Time.deltaTime;
+        }
         m_rigidbody.linearVelocityX = moveVelocityX;
         m_rigidbody.angularVelocity = moveVelocityX * -rotationSpeed;
-        if (moveVelocityY > 0 && canJump)
+
+        if (moveDir.y > 0 && canJump)
         {
+            float moveVelocityY = jumpHeight * Time.deltaTime;
             m_rigidbody.linearVelocityY = moveVelocityY;
             m_spriteRenderer.color = Color.red;
             canJump = false;
@@ -61,6 +75,7 @@ public class BoxScript : MonoBehaviour
 
     public void OnMove(InputValue value)
     {
+
         moveDir = value.Get<Vector2>();
     }
 }
